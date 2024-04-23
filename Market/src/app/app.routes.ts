73696c0d1fe7caab7602af2b070060204b7a86e1
Alishkoo <baisholanov.alibek@gmail.com>
2024-04-23 21:02:01 +0500

    route

--- Market/src/app/app.routes.ts
+++ Market/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AuthComponent } from './auth/auth.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 export const routes: Routes = [
@@ -9,4 +10,5 @@ export const routes: Routes = [
     { path: 'home', component: CategoryListComponent },
     {path : 'products', component : ProductListComponent},
     {path : 'register', component : AuthComponent},
+    {path: 'shopping', component: ShoppingCartComponent},
 ];
