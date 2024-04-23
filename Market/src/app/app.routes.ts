889a8b0d50830aab0be6ad700f927ee2a4fc6433
Nurkenproga <vladus_atabay@mail.ru>
2024-04-23 20:59:36 +0500

    auth

--- Market/src/app/app.routes.ts
+++ Market/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 export const routes: Routes = [
     {path : '', redirectTo : 'home', pathMatch : 'full'},
     { path: 'home', component: CategoryListComponent },
     {path : 'products', component : ProductListComponent},
+    {path : 'register', component : AuthComponent},
 ];
