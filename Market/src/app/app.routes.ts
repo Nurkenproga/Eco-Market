654ee19f085447e16c6d234f263fe763adb72e09
Alishkoo <baisholanov.alibek@gmail.com>
2024-04-23 18:44:14 +0500

    tested

--- Market/src/app/app.routes.ts
+++ Market/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 export const routes: Routes = [
     { path: 'home', component: CategoryListComponent },
+    {path : 'products', component : ProductListComponent},
 ];
