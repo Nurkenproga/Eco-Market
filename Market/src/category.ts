6a0d5039488482412499c6d36253a9de167a8998
Nurkenproga <vladus_atabay@mail.ru>
2024-04-23 16:11:20 +0500

    category_ts

---
+++ Market/src/category.ts
@@ -0,0 +1,38 @@
+export interface Category{
+    id: number;
+    name: string;
+    image: string;
+}
+
+export const Categories: Category[] = [
+    {
+        "id": 1,
+        "name": "Молочные продукты",
+        "image": "https://res.cloudinary.com/neomedtech/image/upload/v1/media/product-category/%D0%BC%D0%BE%D0%BB%D0%BE%D0%BA%D0%BE_c0ojwm"
+      },
+      {
+        "id": 2,
+        "name": "Чай кофе",
+        "image": "https://res.cloudinary.com/neomedtech/image/upload/v1/media/product-category/%D1%87%D0%B0%D0%B8_%D0%BA%D0%BE%D1%84%D0%B5_xhjuuc"
+      },
+      {
+        "id": 3,
+        "name": "Зелень",
+        "image": "https://res.cloudinary.com/neomedtech/image/upload/v1/media/product-category/%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D1%8C_m3krco"
+      },
+      {
+        "id": 4,
+        "name": "Овощи",
+        "image": "https://res.cloudinary.com/neomedtech/image/upload/v1/media/product-category/%D0%BE%D0%B2%D0%BE%D1%89%D0%B8_gqc2e3"
+      },
+      {
+        "id": 5,
+        "name": "Сухофрукты",
+        "image": "https://res.cloudinary.com/neomedtech/image/upload/v1/media/product-category/suxofruits_a9q2mj"
+      },
+      {
+        "id": 6,
+        "name": "Фрукты",
+        "image": "https://res.cloudinary.com/neomedtech/image/upload/v1/media/product-category/%D1%84%D1%80%D1%83%D0%BA%D1%82%D1%8B_i9xd1e"
+      }
+];
