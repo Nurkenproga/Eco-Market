2352d8afc3416a4cc4f32bc8c2d4b598d93bed39
Nurkenproga <vladus_atabay@mail.ru>
2024-04-24 20:09:37 +0500

    worked

--- Market/src/app/app.routes.ts
+++ Market/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { CategoryListComponent } from './category-list/category-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AuthComponent } from './auth/auth.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HistoryComponent } from './history/history.component';
 
 
 export const routes: Routes = [
@@ -11,5 +13,7 @@ export const routes: Routes = [
     {path : 'products', component : ProductListComponent},
     {path : 'register', component : AuthComponent},
     {path: 'shopping', component: ShoppingCartComponent},
-    {path: 'products/:id', component: ProductListComponent}
+    {path: 'products/id', component: ProductDetailsComponent},
+    {path: 'history' , component: HistoryComponent},
+
 ];
