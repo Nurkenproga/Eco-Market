70f9dff99ccccd4f39aa19dc2a68a0a382fd6c2f
Nurkenproga <vladus_atabay@mail.ru>
2024-04-24 17:13:50 +0500

    product list styling

--- Market/src/app/product-list/product-list.component.ts
+++ Market/src/app/product-list/product-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Products } from '../product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
 
+export class ProductListComponent {
+    products = Products;
 }
